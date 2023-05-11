7dbf656b3f85a11d39511ba33de5d4d994a9f5d0
Lounes14 <127855837+Lounes14@users.noreply.github.com>
2023-05-11 13:06:07 +0100

    Add files via upload

--- register.php
+++ register.php
@@ -2,43 +2,194 @@
 <?php include('dbconfig.php') ?>
 <!DOCTYPE html>
 <html>
+	
 <head>
   <title>Registration system PHP and MySQL</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
+  <style type="text/css">
+	body{
+    display: flex;
+    justify-content: center;
+     align-items: center;
+     font-family: 'Roboto', sans-serif;
+     
+    background-image: url("inscrip.jfif") ;
+    
+    background-size:100%;
+   
+    
+    
+
+}
+form{
+    margin-top: 10px;
+    background-color: rgba(255, 255, 255, 0.409);
+    padding: 10px 30px;
+    border-radius: 25px;
+    min-width: 300px;
+}
+
+
+form p.choose-email{
+    text-align: center;
+    color: white;
+    
+
+}
+form .input{
+    display: flex;
+    flex-direction: column;
+   
+
+}
+form .input input[type="email"], input[type ="password"]{
+    padding: 10px;
+    border-radius: 25px;
+    border: none;
+    background-color:white;
+    margin-bottom: 0px;
+    outline: none;
+
+}
+form p.inscription{
+    font-size: 15px;
+    margin-bottom: -10px;
+    color:rgb(9, 8, 8);
+}
+form p.inscription span{
+    color: rgb(3, 3, 3);
+    cursor: pointer;
+}
+form button{
+    display: flex;
+    justify-content: center;
+    justify-items: center;
+    padding: 10px 25px;
+    border: none;
+    border-radius: 10px;
+    font-size: 15px;
+    color: rgb(27, 30, 199);
+    background-color:white;
+     cursor: pointer;
+}
+.button input{
+    width: 35%;
+    padding: 10px 25px;
+    background-color:rgb(27, 30, 199);
+    color:white;
+    font-size: 15px;
+    border: none;
+    border: radius 25px;
+    display: flex;
+    justify-content: center;
+    justify-items: center;
+    transition: all .4 ease-in;
+    margin: auto;
+    display: flex;
+    cursor: pointer;
+}
+h2{
+    
+    display: flex;
+    justify-content: center;
+    justify-items: center;
+    color:rgb(27, 30, 199);
+    font-weight:bold;
+    text-align: center;
+    text-transform: uppercase;
+    padding: -10px;
+
+    
+    
+      
+}
+input{
+    width: 220px;
+    padding: 10px 10px;
+    outline: none;
+    display: flex;
+    border: none;
+    border-radius: 20px;
+    
+  
+
+    
+}
+header {
+    max-width: 80%;
+    margin-right: auto;
+    margin-left: 100px;
+
+
+}
+nav{
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    width: 100%;
+    padding: 20px 0 10px;
+    justify-items: center;
+
+}
+nav .logo{
+    width: 90px;
+    border-radius: 15px;
+    
+    
+
+
+
+}
+
+</style>
 </head>
 <body>
+<div class="formullaire">
+            <div class="barre"></div><br>
+        <form>
+            <header>
+                <nav>
+                   <a href="#"><img src="geo tracer-3.png" alt=""class="logo" srcset=""></a>
+       
+                </nav>
+       
+             </header>
   <div class="header">
   	<h2>Register</h2>
   </div>
 	
-  <form method="post" action="register.php">
+  <form method="post" action="server.php">
   	<?php include('errors.php'); ?>
-  	<div class="input-group">
-  	  <label>Username</label>
-  	  <input type="text" name="username" value="<?php echo $username; ?>">
+  	<div class="form1">
+	  <label for="username">username <span class="star">*</span></label><br>
+      <input type="text" name="username" value="<?php echo $username; ?>"placeholder="votre age"required class="input">
   	</div><br>
-	  <div class="input-group">
+	  <div class="form1">
   	  <label>Name</label>
   	  <input type="text" name="name" value="<?php echo $name; ?>">
   	</div><br>
-  	<div class="input-group">
+  	<div class="form1">
   	  <label>Email</label>
   	  <input type="email" name="email" value="<?php echo $email; ?>">
   	</div><br>
-  	<div class="input-group">
+  	<div class="form1">
   	  <label>Pseudo</label>
   	  <input type="text" name="pseudo" value="<?php echo $pseudo; ?>">
   	</div><br>
-	  <div class="input-group">
+	  <div class="fomr1">
   	  <label>Password</label>
   	  <input type="text" name="password" value="<?php echo $password; ?>">
   	</div><br>
-  	<div class="input-group">
+  	<div class="button">
   	  <button type="submit" class="btn" name="reg_user">Register</button>
   	</div>
   	<p>
   		Already a member? <a href="login.php">Sign in</a>
   	</p>
+	  
+    
+
   </form>
+  
+        
 </body>
 </html>
